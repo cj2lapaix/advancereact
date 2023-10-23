0d6d4052ff339f2db74f28eec8d6b260b091e9e8
Criss Ekoloko <cj2lapaix@gmail.com>
2023-10-22 22:50:17 -0400

    added components modified App.js index.html

---
+++ advancereact1/src/Footer.js
@@ -0,0 +1,33 @@
+function Footer() {
+    return(
+        <footer>
+            <nav>
+                <ul>
+                    <li>Doormat Navigation</li>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Menu</a></li>
+                    <li><a href="">Reservation</a></li>
+                    <li><a href="">Order online</a></li>
+                    <li><a href="">Login</a></li>
+                </ul>
+            </nav>
+            <>
+                <ul>
+                    <li>Contact:</li>
+                    <li>111 5th ave chicago illinois, chicago 2154</li>
+                    <li>555-555-555</li>
+                    <li>littlelemom@littlelemon.com</li>
+                </ul>
+            </>
+            <>
+                <ul>
+                    <li><a>Social Media Links</a></li>
+                    <li><a href="">insta:@Littlelemon</a></li>
+                    <li><a href="">X:@Littlelemon</a></li>
+                </ul>
+            </>
+        </footer>
+    );}
+
+    export default Footer;
---
+++ advancereact1/src/Header.js
@@ -0,0 +1,6 @@
+function Header() {
+    return(
+        <header>homepage</header>
+    );}
+
+    export default Header;
